1c99a2411d496f5237e2976d556246475fdb95db
yanquankun <yanquankun@golaxy.cn>
2020-12-29 15:19:28 +0800

    1.修复下拉框引入tag控件后rootdir编译错误问题 2.下拉框中tag增加2px间距

--- ng-mui/projects/m-ui-lib/select/select.module.ts
+++ ng-mui/projects/m-ui-lib/select/select.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SelectComponent } from './select.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SelectItemComponent } from './select-item.component';
-import { TagModule } from './../tag';
+import { TagModule } from 'm-ui-lib/tag';
 @NgModule({
   declarations: [SelectComponent, SelectItemComponent],
   imports: [
